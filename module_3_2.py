d176ccd7df7ae95f7b345cfe3998f4ef5e66ca9d
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-11-14 19:11:17 +0400

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,14 +1,19 @@
 # def send_email(message, recipient, sender = "university.help@gmail.com"):
 #     if recipient __contains__('@')
 recipient = 'vasyok1337@gmail.com'
-sender = 'vasyok1337@mail.ru'
-
-# print('recipient', recipient.__contains__('@'))
-
-if '@' in recipient and recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'):
-    if '@' in sender and sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'):
-        pass
-    else:
-        print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
-else:
+sender = 'vasyok133@mail.ru'
+if '@' not in recipient:
     print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
+elif '@' not in sender:
+    print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
+elif not (recipient.endswith('.com') or  recipient.endswith('.ru') or recipient.endswith('.net')):
+    print('Окончание Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
+else:
+    print('Первую проверку прошли')
+
+#print(not recipient.endswith('.com')) or not recipient.endswith('.ru') or not recipient.endswith('.net')): #and (sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net')):
+#     pass
+    #if '@' in sender: #and sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'):
+
+   #else:
+        #print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
