8ddd4c6616d8c94e096acedc1efb525f5d84b709
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-11-20 22:43:33 +0400

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,20 +1,32 @@
-data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
-                  ((), [{(2, 'Urban', ('Urban2', 35))}])]
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
 
 
-def sum_numbers_lengths(*args):
-    w = len(args)
-    for i in args:
-        print(args[0])
-    print(type(args))
+def sum_numbers_lengths(data_structure):
+    result = 0
+
+    if isinstance(data_structure, str):
+        result += len(data_structure)
+
+    elif isinstance(data_structure, (int, float)):
+        result += data_structure
+
+    elif isinstance(data_structure, (list, tuple, set)):
+        for item in data_structure:
+            result += sum_numbers_lengths(item)
+
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            result += sum_numbers_lengths(key)
+            result += sum_numbers_lengths(value)
+
+    return result
 
 
-sum_numbers_lengths(data_structure)
-print(data_structure[0], type(data_structure[0]))
-print(data_structure[1], type(data_structure[1]))
-print(data_structure[2])
-print(data_structure[3])
-print(data_structure[4])
-# print(data_structure[5])
-# print(data_structure[6])
-# print(data_structure[7])
+result = sum_numbers_lengths(data_structure)
+print(result)
